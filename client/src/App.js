ced2cae45f1d8a9f52529b03402c4695f67c1a39
Brian Thomas <Brianthomas571@gmail.com>
2019-02-18 17:58:12 -0600

    added styling and simple html with an image component

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,18 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
+      <div id="root">
+        <div className="hero is-fullheight is-bold is-info">
+          <div className="hero-body">
+            <div className="container">
+              <div className="header-content">
+                <h2 className="subtitle is-6">Code Challenge</h2>
+                <h1 className="title is-1">Infinite Scroll Code Challenge</h1>
+              </div>
+              <Images />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ client/src/components/Image.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Image({ image }) {
+  return (
+    <img src="{image.urls.thumb}" alt="single-photo" className="single-photo" />
+  );
+}
